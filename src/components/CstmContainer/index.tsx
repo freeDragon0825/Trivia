f08ecd5f18b8de7e2e6796f87d8196e2b78e7d9c
Kevin Mitnic <superdevv0292@gmail.com>
2022-09-08 00:11:26 +0900

    Update 'CstmContainer' component.

--- src/components/CstmContainer/index.tsx
+++ src/components/CstmContainer/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { Container } from '@mui/material';
 
-const CustomContainer = ({ ...props }) => {
-  return <Container maxWidth="sm" sx={{ height: '100vh' }} {...props} />;
+const CstmContainer = ({ ...props }) => {
+  return (
+    <Container
+      maxWidth="sm"
+      sx={{ height: '100vh' }}
+      {...props}
+    />
+  );
 };
 
-export default CustomContainer;
+export default CstmContainer;
