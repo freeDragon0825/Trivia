b7bc93995bab40d49dafc4a5b3685f698add04d5
Kevin Mitnic <superdevv0292@gmail.com>
2022-09-07 19:58:40 +0900

    Remove 'Layout' component.

--- src/components/Layout/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Container } from '@mui/material'
-
-const Layout = (props: object) => {
-  return <Container maxWidth="sm" sx={{ height: '100vh' }} {...props} />;
-};
-
-export default Layout;
