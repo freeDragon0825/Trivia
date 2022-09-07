94169499cf4e74399a3a9661b6df012da4be2b47
Kevin Mitnic <superdevv0292@gmail.com>
2022-09-08 00:10:44 +0900

    Update 'CstmBtn' component.

--- src/components/CstmBtn/index.tsx
+++ src/components/CstmBtn/index.tsx
@@ -2,5 +2,12 @@ import React from 'react';
 import Button from '@mui/material/Button';
 
 export default function CstmBtn({ ...props }): JSX.Element {
-  return <Button sx={{ mb: '10px' }} fullWidth variant='contained' {...props} />;
+  return (
+    <Button
+      sx={{ mb: '10px' }}
+      fullWidth
+      variant="contained"
+      {...props}
+    />
+  );
 }
