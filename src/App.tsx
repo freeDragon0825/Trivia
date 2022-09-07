4b0bd9d6cc4bc69a579f9e17b5c5bf3f7cc65584
Kevin Mitnic <superdevv0292@gmail.com>
2022-09-07 19:19:30 +0900

    Update CstmContainer component

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,23 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
+import CstmContainer from 'components/CstmContainer';
 import Home from './pages/Home';
 import Quiz from './pages/Quiz';
 import store from 'store';
 import './App.css';
-import CustomContainer from 'components/CustomContainer';
 
 function App() {
   return (
     <Provider store={store}>
-      <CustomContainer>
+      <CstmContainer>
         <Routes>
           <Route path="/">
             <Route index element={<Home />} />
             <Route path="quiz" element={<Quiz />} />
           </Route>
         </Routes>
-      </CustomContainer>
+      </CstmContainer>
     </Provider>
   );
 }
